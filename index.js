8b6f0daca51c7fcc5aecf3465e35e6e2f0da780d
Pedro Seibel <64672223+pedroseibel@users.noreply.github.com>
2021-10-20 16:47:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const path = require("path");
 const Poema = require("./models/poema");
 app.set("view engine", "ejs");
